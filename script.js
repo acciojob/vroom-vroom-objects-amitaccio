9d6c40e795646dbe25969045aa798a21e3b28075
amitaccio <142372667+amitaccio@users.noreply.github.com>
2023-12-18 10:40:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+    // Call the Car constructor to initialize make and model
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
+}
+
+SportsCar.prototype.getTopSpeed = function(){
+	return this.topSpeed;
+}
+
+SportsCar.prototype.__proto__ = Car.prototype;
 
 // Do not change the code below
 window.Car = Car;
